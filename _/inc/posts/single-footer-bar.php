a4bb7e69d37b0cc3b60689bf651707a9061b6ecc
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-01-20 11:25:30 +0000

    Fix space on freephone number.

--- _/inc/posts/single-footer-bar.php
+++ _/inc/posts/single-footer-bar.php
@@ -1,4 +1,4 @@
-<p class="tel-num">Call us <span>free<a href="tel:<?php echo str_replace(' ', '', $freephone_num); ?>" onclick="ga('send', 'event','Freephone click', 'tap', '<?php echo $post->post_title; ?> - Call back')" title="Call us now"><?php echo $freephone_num; ?></a></span></p>
+<p class="tel-num">Call us <span>free <a href="tel:<?php echo str_replace(' ', '', $freephone_num); ?>" onclick="ga('send', 'event','Freephone click', 'tap', '<?php echo $post->post_title; ?> - Call back')" title="Call us now"><?php echo $freephone_num; ?></a></span></p>
 
 <?php 
 $newsletter_pg = get_page_by_title("Join our mailing list");
