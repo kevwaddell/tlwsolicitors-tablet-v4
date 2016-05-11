0c87ff7b98985cee8d020cfe9b51e7fd98d87b3e
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-05-11 11:29:35 +0100

    Add form to general page template.

--- page.php
+++ page.php
@@ -1,13 +1,18 @@
 <?php get_header(); ?>
+	
+	<?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>	
+	<?php if ( has_post_thumbnail() ) { ?>
+	<?php include (STYLESHEETPATH . '/_/inc/pages/wide-feat-img.php'); ?>
+	<?php } ?>
 	<!-- MAIN CONTENT START -->
 	<div class="container-fluid">
 	
 		<div class="content">
-
-			<?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>	
 			<?php 
 			$related_pages = get_field('page_links'); 
+			$form_active = get_field('form_activated');
 			$hide_title = get_field('hide_title'); 
+			$number_pos = get_field('tel_num_position');
 			?>	
 			 <main class="page-col-red animated fadeIn">
 			 	<div class="row">
@@ -25,6 +30,17 @@
 								<div class="main-txt">
 									<?php the_content(); ?>
 								</div>
+								
+								<?php if ($form_active) : 
+								$form = get_field('form');	
+								?>
+								<div class="contact-form">
+									<div class="well well-lg">
+									<?php gravity_form($form->id, false, false, false, null, true); ?>	
+									</div>		
+								</div>	
+								
+								<?php endif; ?>
 							
 							</article>
 						
@@ -34,11 +50,11 @@
 			 	
 			 </main>
 					
-			<?php endwhile; ?>
-			<?php endif; ?>
+
 
 		</div><!-- CONTENT END -->
 		
 	</div><!-- MAIN CONTENT CONTAINER END -->
-
+	<?php endwhile; ?>
+	<?php endif; ?>
 <?php get_footer(); ?>
