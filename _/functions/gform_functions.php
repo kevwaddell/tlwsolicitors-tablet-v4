1b3ba98dc1d5e8b7e49f4691f9a8bf918b5fd31f
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-09-13 16:03:48 +0100

    Save google click id to cookie and populate form field if it exists.

--- _/functions/gform_functions.php
+++ _/functions/gform_functions.php
@@ -6,4 +6,20 @@ function tgm_io_custom_gforms_spinner( $src ) {
     return get_stylesheet_directory_uri() . '/_/img/ajax-loader.gif';
     
 }
+
+add_filter("gform_field_value_src", "populate_affiliate");
+
+function populate_affiliate($value){
+	if ( isset($_COOKIE['src']) ) {
+	return $_COOKIE["src"];	
+	}
+}
+
+add_filter("gform_field_value_gclid", "populate_gclid");
+
+function populate_gclid($value){
+	if ( isset($_COOKIE['gclid']) ) {
+	return $_COOKIE["gclid"];	
+	}
+}
 ?>
